033906f1f71ac1073effde4ea1ecfb54344099e0
yoo20370 <yoo20370@gmail.com>
2025-04-03 02:15:19 +0900

    [feat] AccessToken 검증 및 RefreshToken를 통한 AccessToken 발급하는 JwtAuthenticationFilter 구현

--- src/main/java/com/MooBoo/MooBoo_Spring/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/MooBoo/MooBoo_Spring/common/filter/JwtAuthenticationFilter.java
@@ -1,24 +1,106 @@
 package com.MooBoo.MooBoo_Spring.common.filter;
 
+import com.MooBoo.MooBoo_Spring.application.port.inbound.bookapi.RefreshTokenService;
+import com.MooBoo.MooBoo_Spring.application.port.outbound.external.jwt.JwtProvider;
+import com.MooBoo.MooBoo_Spring.domain.TokenStatus;
+import com.MooBoo.MooBoo_Spring.domain.refreshtoken.RefreshToken;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 /**
- *
+ * 요청이 들어오면 해당 필터가 요청을 가로채 토큰 검증
  */
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = extractAccessToken(request);
+        TokenStatus tokenStatus = jwtProvider.validateToken(accessToken);
 
+        if (accessToken != null && tokenStatus == TokenStatus.VALID) {
+            Authentication authentication = jwtProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
+        } else if (accessToken != null && tokenStatus == TokenStatus.EXPIRED) {
+            String refreshToken = extractRefreshToken(request);
+
+            Optional<RefreshToken> result = refreshTokenService.find(refreshToken);
+            if (refreshToken != null && !result.isEmpty() && result.get().validate(refreshToken)) {
+                String userId = jwtProvider.getUserId(accessToken);
+                List<String> roles = jwtProvider.getRoles(accessToken);
+                String nickName = jwtProvider.getNickName(accessToken);
+                accessToken = jwtProvider.createAccessToken(userId, roles, nickName);
+
+                Authentication authentication = jwtProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            } else {
+                redirectToLogin(response);
+                return;
+            }
+
+        } else {
+            redirectToLogin(response);
+            return;
+        }
+        // 계속 진행
+        filterChain.doFilter(request, response);
+    }
+
+    private static void redirectToLogin(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+        {
+          "message": "인증 필요",
+          "code": "Unauthorized"
+        }
+        """);
+    }
+
+    private String extractAccessToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+
+        return "";
+    }
+
+    private String extractRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) return "";
+
+        for (Cookie cookie : cookies) {
+            if ("refreshToken".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return "";
     }
 }
