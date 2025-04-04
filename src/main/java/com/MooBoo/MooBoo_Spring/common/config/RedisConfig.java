e121e12514eb866dea13c2b19cde64ae135e4029
yoo20370 <yoo20370@gmail.com>
2025-04-05 01:19:26 +0900

    [feat] RedisConfig 파일 설정

---
+++ src/main/java/com/MooBoo/MooBoo_Spring/common/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.MooBoo.MooBoo_Spring.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // JSON 직렬화 설정
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        template.setDefaultSerializer(serializer);
+        return template;
+    }
+}
